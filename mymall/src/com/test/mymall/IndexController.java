132831770cdf60ca453875d3968b8b1d8f91514b
devwondev <won3055@gmail.com>
2018-10-26 14:56:58 +0900

    [방혜원] mymall 웹프로젝트 초기화
    1. "/"요청
    2. jstl 라이브러리 추가

---
+++ mymall/src/com/test/mymall/IndexController.java
@@ -0,0 +1,18 @@
+package com.test.mymall;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/IndexController")
+public class IndexController extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("IndexController.doGet()");
+		request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request,response);
+	}
+
+}
