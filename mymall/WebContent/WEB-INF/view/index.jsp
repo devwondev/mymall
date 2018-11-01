b90efcd1e78212eee6e47c70ee232ca461ecd147
devwondev <won3055@gmail.com>
2018-11-01 09:08:38 +0900

    [방혜원] 로그인, 로그아웃 구현
    1. MemberDao에 로그인처리 메서드 구현
    2. 로그인 후 index.jsp 구현
    3. index.jsp에서 로그아웃 버튼 클릭 시 세션종료

--- mymall/WebContent/WEB-INF/view/index.jsp
+++ mymall/WebContent/WEB-INF/view/index.jsp
@@ -8,6 +8,13 @@
 </head>
 <body>
 	<h1>index</h1>
-	<a href="${pageContext.request.contextPath}/AddMemberController">회원가입</a>
+	<c:if test="${loginMember == null}">
+		<a href="${pageContext.request.contextPath}/AddMemberController">회원가입</a>
+		<a href="${pageContext.request.contextPath}/LoginController">로그인</a>
+	</c:if>
+	<c:if test="${loginMember != null}">
+		${loginMember}님 반갑습니다.
+		<a href="${pageContext.request.contextPath}/LogoutController">로그아웃</a>
+	</c:if>
 </body>
 </html>
--- mymall/src/com/test/mymall/web/LoginController.java
+++ mymall/src/com/test/mymall/web/LoginController.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.test.mymall.dao.MemberDao;
 import com.test.mymall.vo.Member;
@@ -20,13 +21,29 @@ public class LoginController extends HttpServlet {
 			request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
 		} else {
 			System.out.println("로그인 중입니다");
-			response.sendRedirect("/mymall/index");
+			response.sendRedirect(request.getContextPath()+"/index");
 		}
 	}
 	// 로그인 처리
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("LoginController.doPost()");
+		boolean isLogin = false;
+		Member member = new Member();
+		memberDao = new MemberDao();
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		member.setId(id);
+		member.setPw(pw);
+		isLogin = memberDao.loginMember(member);
 		
+		if(isLogin) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginMember", member.getId());
+			response.sendRedirect(request.getContextPath()+"/index");
+		}
+		else {
+			response.sendRedirect(request.getContextPath()+"/login");
+		}
 	}
 
 }
